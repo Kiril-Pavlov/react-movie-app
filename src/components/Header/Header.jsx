7209746611991342eb396f6dcc4412c9c8c82521
Kiril-Pavlov <Kire_P@yahoo.com>
2022-08-29 20:26:06 +0200

    Header component created with basic CSS

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+        <h1 className='header-title'>Kiril's Movie Site</h1>
+    </div>
+  )
+}
+
+export default Header
