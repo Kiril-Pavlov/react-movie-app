026a01215a511f89ae862c824c9a4ed571beda16
Kiril-Pavlov <Kire_P@yahoo.com>
2022-08-30 21:37:19 +0200

    refactor on Home component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,31 +6,34 @@ import Movie from "./Movie";
 import "./Home.css"
 
 const Home = () => {
-  const [movies,setMovies] = useState([]);
-  const [apiURL,setApiURL] = useState('/api/v2/list_movies.json');
+  const [movies, setMovies] = useState([]);
+  const [apiURL, setApiURL] = useState('/api/v2/list_movies.json');
 
   useEffect(()=>{
-    axios.get(apiURL).then((response)=>{
+    getMovies(apiURL);
+    },[])
+
+  const getMovies = (apiURL) => {
+    axios.get(apiURL).then((response) => {
       console.log(response)
       setMovies(response.data.data.movies)
-    }).catch((error)=>{
+    }).catch((error) => {
       console.log(error)
     })
-
-  },[])
+  }
 
     console.log(movies)
 
 
   return (
-    <div className='home-container'>
-      {movies.map(movie => <Movie key={movie.id} 
-      movieTitle={movie.title} 
-      movieImg={movie.medium_cover_image} 
-      movieGenre={movie.genres} 
-      movieRating={movie.rating} />)}
-    </div>
-  )
-}
+      <div className='home-container'>
+        {movies.map(movie => <Movie key={movie.id}
+          movieTitle={movie.title}
+          movieImg={movie.medium_cover_image}
+          movieGenre={movie.genres}
+          movieRating={movie.rating} />)}
+      </div>
+    )
+  }
 
-export default Home
+  export default Home
