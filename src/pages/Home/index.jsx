6e7c332f596d284469e2ed0364f128f694395b51
Kiril-Pavlov <Kire_P@yahoo.com>
2022-08-30 21:17:59 +0200

    cors error fixed

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import "./Home.css"
 
 const Home = () => {
+  const [movies,setMovies] = useState([]);
+  const [apiURL,setApiURL] = useState('/api/v2/list_movies.json');
+
+  // const cors = require("cors");
+  // app.use(
+  //   cors({
+  //     origin: "https://yts.torrentbay.to/api/v2/list_movies.json",
+  //   })
+  // )
+
+  useEffect(()=>{
+    axios.get(apiURL).then((response)=>{
+      console.log(response.data.data)
+    }).catch((error)=>{
+      console.log(error)
+    })
+
+  },[])
+
+
+
   return (
     <div>Home</div>
   )
