c46b211040858879ea17f2eead90950906159f6b
Kiril-Pavlov <Kire_P@yahoo.com>
2022-08-31 21:36:08 +0200

    Movie component CSS done

--- src/pages/Home/Movie.jsx
+++ src/pages/Home/Movie.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
+import "./Movie.css"
+
 function Movie(props) {
 
-
-    return <div className="item">
+    return <div className="movie-item">
         <img src={props.movieImg} alt={props.movieTitle} /><br />
         <div className="movie-title">{props.movieTitle}</div>
         <div className="movie-details">
-            <div className="movie-genres">{props.movieGenre}</div>
-            <div className="movie-rating">{props.movieRating}</div>
+            <div className="movie-genres">{props.movieGenre.join(" ")}</div>
+            <div className={props.movieRating>7 ? "movie-rating green": "movie-rating red"}>{props.movieRating}</div>
         </div>
 
     </div>
